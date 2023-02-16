a88a372fef1f63dd1fcb55c5d09fd950b71dc22e
Shubham <shubhamlahase@hmail.com>
2023-02-16 13:00:40 +0530

    created app basic layout with grid

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from "react";
+import './App.css';
+import Header from "./Header/Header";
 
 function App() {
   return (
     <div className="App">
       <>
-        Cleared CRA Template
+        <div class="container">
+          <div class="App-Header"><Header/></div>
+          <div class="App-Body"></div>
+        </div>
       </>
     </div>
   );
