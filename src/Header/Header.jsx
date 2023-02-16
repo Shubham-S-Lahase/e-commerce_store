1f7d2779b87be01bc791b6b4db7b32ab64e27c53
Shubham <shubhamlahase@hmail.com>
2023-02-16 14:51:22 +0530

    completed conditional rendering of dropdown menu and app title and cart icon

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -1,22 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Header.css";
 
 const Header = () => {
-
+  const [open, setOpen] = useState(false);
+  let countclick = 0;
+  const handleClick = () => {
+    setOpen(true);
+  };
+  const handleMouseDown = () => {
+    setOpen(false);
+  };
   return (
     <>
       <div className="container-header">
         <div className="container-menu">
-          <button type="button" class="button">
+          <button type="button" className="button" onClick={handleClick}>
             ☰
           </button>
-          <div class="dropdown">
-            <ul>
-              <li>Option 1</li>
-              <li>Option 2</li>
-              <li>Option 3</li>
-              <li>Option 4</li>
-            </ul>
+          {open && (
+            <>
+              <div className="dropdown">
+                <ul>
+                  <li>Option 1</li>
+                  <li>Option 2</li>
+                  <li>Option 3</li>
+                  <li>Option 4</li>
+                </ul>
+              </div>
+            </>
+          )}
+        </div>
+        <div className="TC" onMouseDown={handleMouseDown}>
+          <div className="app-title">
+            <h2>Anavrin</h2>
+            <p>Video Games & Fan Gear</p>
+          </div>
+          <div className="cart-logo">
+            <img src={require("./R.png")}/>
           </div>
         </div>
       </div>
